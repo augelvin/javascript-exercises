5f3989711739b88df57ba399e4ce1d031d818c68
Augelvin Theone Dermaga <augelvintd@gmail.com>
2022-11-19 13:41:07 -0800

    add function to return reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    /* split each character of the string into an array */
+    const chars = string.split('');
+
+    /* reverse the order of the array */
+    const reversedChars = [];
+    for (char of chars) {
+        reversedChars.unshift(char);
+    }
+
+    /* return reversed array as a string */
+    return reversedChars.join('')
 
 };
 
