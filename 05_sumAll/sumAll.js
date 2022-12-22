f50b766246fb131d8f57fe0843d872dd46fed391
Augelvin Theone Dermaga <augelvintd@gmail.com>
2022-12-21 21:16:28 -0800

    add conditional if b is bigger than a

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,16 @@ const sumAll = function(a, b) {
     let sum = 0;
     if (a < 0 || b < 0 ) {
         return "ERROR"
+    } else if (a > b) {
+        for (let i = b; i < a; i++) {
+            sum += i;
+        }
+        return sum
+    } else if (b > a) {
+        for (let i = a; i < b; i++) {
+            sum += i;
+        }
+        return sum
     }
 };
 
