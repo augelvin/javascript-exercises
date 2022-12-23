942a124e24bd58c450325fc05bc944a6f796569f
Augelvin Theone Dermaga <augelvintd@gmail.com>
2022-12-23 15:26:07 -0800

    add condition if year is divisible by 4 and else

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,7 +3,11 @@ const leapYears = function(year) {
         return true
     } else if (year % 100 == 0) {
         return false
-    } 
+    } else if (year % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
