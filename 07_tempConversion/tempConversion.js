25489be59e8a25335521af30eedfab58e372ee9f
Augelvin Theone Dermaga <augelvintd@gmail.com>
2022-12-23 16:27:37 -0800

    add celcius to fahrenheit calculation

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
 const convertToCelsius = function(f) {
   let celcius = (f - 32) * 5/9;
-  celcius = Math.round(celius * 10) /10;
+  celcius = Math.round(celius * 10) / 10;
   return celcius
 };
 
 const convertToFahrenheit = function(c) {
+  let fahrenheit = (c * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit
 };
 
 // Do not edit below this line
