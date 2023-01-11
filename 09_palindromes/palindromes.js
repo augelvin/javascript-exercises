17518849f06fe3a04fda1bffc07c86b316806b97
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-01-10 20:46:22 -0800

    add funciton to check palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const plain = text.toLowerCase().replace(/[.,?! ]/g,""); // remove punctuations and whitespaces
+    const array = [...plain]; // spread
+    const reversed = array.reverse().join(''); // reverse and join array
+    return plain == reversed
 
 };
 
