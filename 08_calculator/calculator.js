0c4a41fbb1ff6f3d37776a376c05f6adac4a0a30
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-01-10 20:07:46 -0800

    add factorial calculation

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,14 @@ const power = function(a, b) {
 	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  const array = [];
+  while (a > 0) {
+    array.push(a);
+    a -= 1;
+  };
+
+  return array.reduce((inter, i) => inter * i, 1)
 };
 
 // Do not edit below this line
