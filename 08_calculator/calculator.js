8a79fef47e6753109d91a6f6acd40d624b1afc3c
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-01-10 19:56:34 -0800

    add multiply calculation

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(array) {
   return array.reduce((inter, i) => inter + i, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((inter, i) => inter * i, 1);
 };
 
 const power = function() {
