c4a65906beaca72204164489912bfc74e7860198
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-01-21 13:56:13 -0800

    add function to get oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // sort according to age
+    const ordered = people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
 
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+
+        if (a.yearOfDeath-a.yearOfBirth > b.yearOfDeath- b.yearOfBirth) {
+            return -1
+        } else {
+            return 1
+        }
+    });
+    
+    // return the oldest
+    return ordered[0]
 };
 
 // Do not edit below this line
